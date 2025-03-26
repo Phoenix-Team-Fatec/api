04ae0036f09fa6cf4897b10b121944d15c7079db
João Victor <144742430+jvictormo@users.noreply.github.com>
2025-03-26 15:26:33 -0300

    Fixing uploads folder not appering

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,6 +1,12 @@
-// src/middlewares/upload.ts
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadPath = path.join(__dirname, "..", "uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
