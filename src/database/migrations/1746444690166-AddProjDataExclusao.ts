c37174298b5776b241d84b930a39de1786c7fff4
TaldoGus <xgustavopereirax@gmail.com>
2025-05-19 08:33:33 -0300

    Correção de migration

--- src/database/migrations/1746444690166-AddProjDataExclusao.ts
+++ src/database/migrations/1746444690166-AddProjDataExclusao.ts
@@ -3,11 +3,17 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class AddProjDataExclusao1746444690166 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        const projetoTable = await queryRunner.getTable("projeto");
+        if (!projetoTable.columns.find(c => c.name === "proj_data_exclusao")) {
         await queryRunner.query(`ALTER TABLE "projeto" ADD "proj_data_exclusao" TIMESTAMP`);
+    }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        const projetoTable = await queryRunner.getTable("projeto");
+        if (!projetoTable.columns.find(c => c.name === "proj_data_exclusao")) {
         await queryRunner.query(`ALTER TABLE "projeto" DROP COLUMN "proj_data_exclusao"`);
+    }
     }
 
 }
